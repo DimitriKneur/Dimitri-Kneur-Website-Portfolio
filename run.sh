7455e9f5d8e9e7b4fd84b1fbc345ab58dd1e48b5
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 11:08:22 +0200

    j

--- run.sh
+++
@@ -1,1 +0,0 @@
-streamlit run 🏠_Home.py
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title("Home Page")
-st.write("Welcome to the home page!")
+st.write(f"""<div class="title"><strong>Welcome to my professional website👋</div>""", unsafe_allow_html=True)
 
+st.write(f"""
+<div style="display: flex; justify-content: center;">
+<img src="assets/profile_picture.png" alt="Dimitri Kneur" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+</div>
+""", unsafe_allow_html=True)
