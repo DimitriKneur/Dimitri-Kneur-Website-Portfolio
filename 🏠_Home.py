9c77f12857d1f1029f554860f6d933e0a6fe1131
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 11:18:42 +0200

    center

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
-st.title("Welcome to my website 👋🏽")
-
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
+    st.title("Welcome to my website 👋🏽")
     st.image('assets/profile_picture.png', width=300)
-
-st.write("Dimitri Kneur")
-
+    st.write("Dimitri Kneur")
