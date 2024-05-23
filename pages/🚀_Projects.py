8261bfc6cf5c96d4a3d3361a08f5faa2cac3ed60
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 09:51:55 +0200

    l

--- pages/🚀_Projects.py
+++ pages/🚀_Projects.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-def page_projects():
-    st.title("Projects Page")
-    st.write("Here are some of my projects.")
+st.title("Projects Page")
+st.write("Here are some of my projects.")
