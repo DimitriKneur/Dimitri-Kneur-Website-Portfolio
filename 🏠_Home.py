43ec2895ed2f83a0cb672d7b0a74acbbb7ae3423
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 10:42:14 +0200

    m

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import os
+
+# Define the path to your CSS file
+css_file_path = "styles.css"
+
+# Function to load the CSS file and inject it into the app
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Load the CSS file
+load_css(css_file_path)
 
 st.title("Home Page")
 st.write("Welcome to the home page!")
