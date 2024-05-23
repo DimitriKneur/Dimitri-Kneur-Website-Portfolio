43a7bd498175ca20867bb97fbaabb692e55dd454
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 11:21:20 +0200

    c

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
+st.markdown("<h1 style='text-align: center; color: black;'>Welcome to my website 👋🏽</h1>", unsafe_allow_html=True)
+
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
-    st.title("Welcome to my website 👋🏽")
     st.image('assets/profile_picture.png', width=300)
-    st.write("Dimitri Kneur")
