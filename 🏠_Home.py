34aab031f04adf29d30a7929484d78e5ecbcf994
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-23 11:12:21 +0200

    a

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.write(f"""<div class="title"><strong>Welcome to my professional website👋</div>""", unsafe_allow_html=True)
+st.title("Welcome to my professional website 👋🏽")
 
-st.write(f"""
-<div style="display: flex; justify-content: center;">
-<img src="assets/profile_picture.png" alt="Dimitri Kneur" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
-</div>
-""", unsafe_allow_html=True)
+st.image('assets/profile_picture.png')
+
+st.write("Dimitri Kneur")
+
