684c185a9f782de92681415f0ad934b521d7c833
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-24 22:47:51 +0200

    update_projects

--- pages/04_🧪_Projects.py
+++ pages/04_🧪_Projects.py
@@ -1,23 +1,82 @@
 import streamlit as st
 
 st.set_page_config(page_title="My Portfolio Website", 
-                   page_icon=":rocket:")
+                   page_icon=":rocket:",
+                   layout="wide")
 
 st.header("Projects", divider="red")
 
-col1, col2 = st.columns((1, 0.86))
+st.markdown("""
+    <h3 style='text-align: center; color: black;'>
+        ¤
+        <a href="https://github.com/DimitriKneur/Global-Electronics-Retailer-Analysis">
+            Interactive Power BI sales dashboard
+        </a>
+    </h3>
+    """, unsafe_allow_html=True)
+
+
+col1, col2, col3 = st.columns((1, 3.2, 1))
 
 with col1:
-    st.image("assets/Retail_Dashboard_Snapshot.gif", caption="Interactive Power BI dashboard / Global Electronics Retailer")
-    st.image("assets/Netflix_Catalog_Presentation_Snapshot.gif", caption="Analyze the catalog of Netflix with Power BI")
+    st.write(' ')
 
 with col2:
-    st.image("assets/Demo_streamlit_le_louvre.gif", caption = "Application listing the artworks at the Louvre Museum")
-    st.image("assets/World_Population_Evolution_Snapshot.gif", caption="Analyze the world population's evolution (2000 - 2021)")
+    st.image("assets/Retail_Dashboard_Snapshot.gif")
+
+with col3:
+    st.write(' ')
+
+st.write(' ')
+
+st.markdown("""<h3 style='text-align: center; color: black;'>¤ <a href="https://github.com/DimitriKneur/Netflix-Catalog-Analysis-PowerBI">Analysis of the catalog of Netflix with Power BI</a></h3>""", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns((1, 3.2, 1))
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("assets/Netflix_Catalog_Presentation_Snapshot.gif")
+
+with col3:
+    st.write(' ')
+
+st.write(' ')
+
+st.markdown("""<h3 style='text-align: center; color: black;'>¤ <a href="https://github.com/DimitriKneur/World-Population-Evolution-Project/tree/main">Analysis of the world population's evolution</a></h3>""", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns((1, 3.2, 1))
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("assets/World_Population_Evolution_Snapshot.gif")
+
+with col3:
+    st.write(' ')
+
+st.write(' ')
+
+st.markdown("""<h3 style='text-align: center; color: black;'>¤ <a href="https://github.com/DimitriKneur/Le-Louvre-works-of-art-on-display/tree/main">Application listing the artworks at the Louvre Museum</a></h3>""", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns((1, 3.2, 1))
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("assets/Demo_streamlit_le_louvre.gif")
+
+with col3:
+    st.write(' ')
+
+st.write(' ')
 
 st.markdown("""
-    <p style='text-align: center; color: black;'>
+    <h3 style='text-align: center; color: black;'>
         Do you want to have a closer look at all my projects? 
         Follow this <a href="https://github.com/DimitriKneur/Data-Projects/blob/main/README.md">link</a>.
-    </p>
+    </h3>
 """, unsafe_allow_html=True)
